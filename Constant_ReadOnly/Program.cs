c8949894071bd8ca44a1110401f383ecc2868a3e
hasher116 <andrei13081996@mail.ru>
2021-08-03 18:09:44 +0300

    Добавьте файлы проекта.

---
+++ Constant_ReadOnly/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Constant_ReadOnly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MathLib.PI);
+
+            MathLib abc = new MathLib();
+            Console.WriteLine(abc.PI2);
+
+            Console.WriteLine(abc.MathLib2());
+        }
+    }
+
+     class MathLib
+    {
+        private decimal squad;
+        private double area;
+        public const double PI = 3.1;
+        public readonly double PI2 = 3.14;
+        public MathLib()
+        {
+            PI2 = 5;
+            area = PI2;
+        }
+        public decimal MathLib2()
+        {
+            return squad = (decimal) (Math.Pow(PI, 2) / 2);
+        }
+    }
+}
